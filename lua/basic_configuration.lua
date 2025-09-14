61d45bc38dfb403f9f45db5b26e6b7acccda7d7e
Simen Holmestad <simen@holmestad.com>
2025-09-14 12:18:12 +0200

    Begynn å dele opp konfigurasjonen

---
+++ lua/basic_configuration.lua
@@ -0,0 +1,124 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+
+vim.opt.number = true
+-- vim.opt.reltivenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Enable autochdir so that the current file in buffer is always the working directory
+vim.opt.autochdir = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+--  Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<leader>w', '<C-w>')
+vim.keymap.set('n', '<leader>wd', '<C-w>q')
+vim.keymap.set('n', '-', '/')
+
+vim.api.nvim_set_keymap('n', '<leader>fs', ':w<Enter>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<C-x><C-c>', ':q!<Enter>', { silent = true })
+
+vim.keymap.set('n', '<leader>ss', '/', { desc = '[S]earch [S] current file' })
+
+vim.api.nvim_set_keymap('n', 's', '/', { silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>ff', ':e.<Enter>', { silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>en', ':tabnew<Enter>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>el', 'gt', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>eh', 'gT', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ed', ':tabclose<Enter>', { noremap = true, silent = true })
+
+-- Se https://neovide.dev/configuration.html for mer konfigurasjon
+vim.g.neovide_macos_simple_fullscreen = true
+vim.g.neovide_cursor_animation_length = 0
+vim.api.nvim_set_keymap('n', '<c-ø>', ':let g:neovide_macos_simple_fullscreen = v:false<Enter>', { noremap = true, silent = true })
+vim.g.neovide_scroll_animation_length = 0.2
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
