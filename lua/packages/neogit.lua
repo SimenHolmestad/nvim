719d84f004c945f24aad20c50e9af3a404d142b6
Simen Holmestad <simen@holmestad.com>
2025-09-14 21:23:01 +0200

    Neogit config

--- lua/packages/neogit.lua
+++ lua/packages/neogit.lua
@@ -8,6 +8,10 @@ return {
       'nvim-telescope/telescope.nvim',
     },
     config = function()
+      require('neogit').setup {
+        disable_hint = true,
+        kind = 'vsplit',
+      }
       vim.api.nvim_set_keymap('n', '<leader>gg', ':Neogit<Enter>', { noremap = true, silent = true })
     end,
   },
