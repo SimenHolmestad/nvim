1edf611d933143b98bbd4d7c2e073eec85d415fa
Simen Holmestad <simen@holmestad.com>
2025-09-17 19:37:44 +0200

    Forbedret neogit

--- lua/packages/neogit.lua
+++ lua/packages/neogit.lua
@@ -10,29 +10,34 @@ return {
     config = function()
       require('neogit').setup {
         disable_hint = true,
+        auto_close_console = false,
         auto_refresh = true,
         kind = 'replace',
         commit_editor = {
-          kind = 'replace',
-          staged_diff_split_kind = 'auto',
+          kind = 'vsplit',
+          show_staged_diff = false,
         },
         commit_select_view = {
           kind = 'replace',
         },
+        commit_view = {
+          kind = 'replace',
+          verify_commit = vim.fn.executable 'gpg' == 1,
+        },
         log_view = {
           kind = 'replace',
         },
+        stash = {
+          kind = 'vsplit',
+        },
+        mappings = {
+          popup = {
+            ['h'] = 'HelpPopup',
+            ['F'] = 'PullPopup',
+          },
+        },
       }
       vim.api.nvim_set_keymap('n', '<leader>gg', ':Neogit<Enter>', { noremap = true, silent = true })
-
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'NeogitCommitComplete',
-        desc = 'Move back to Neogit status buffer after creating commit',
-        callback = function()
-          vim.cmd 'bd'
-          vim.cmd 'Neogit'
-        end,
-      })
     end,
   },
 }
