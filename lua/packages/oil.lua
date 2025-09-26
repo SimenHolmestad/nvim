3ed7d6f4e206ceeb5433ed40cc17f6ca6fed8bff
Simen Holmestad <simen@holmestad.com>
2025-09-26 19:44:19 +0200

    Bytt directory i Oil buffer

--- lua/packages/oil.lua
+++ lua/packages/oil.lua
@@ -16,6 +16,18 @@ return {
         },
       }
       vim.keymap.set('n', '<leader>ff', ':Oil<Enter>', { noremap = true, silent = true })
+
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = '*',
+        desc = 'Change cwd to dir in oil buffer',
+        callback = function()
+          local oil_buffer_path = vim.fn.expand '%:p'
+          local buffer_file_path = oil_buffer_path:gsub('oil://', '')
+          if vim.fn.isdirectory(buffer_file_path) == 1 then
+            vim.cmd('cd ' .. buffer_file_path)
+          end
+        end,
+      })
     end,
   },
 }
