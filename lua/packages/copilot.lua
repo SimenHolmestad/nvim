2e916359dc4fe8a58d58c7ea235688194d213c81
Simen Holmestad <simen@holmestad.com>
2025-09-29 20:38:15 +0200

    Toggle copilot

--- lua/packages/copilot.lua
+++ lua/packages/copilot.lua
@@ -16,7 +16,7 @@ return {
 
     -- Custom keymaps
     local suggestion = require 'copilot.suggestion'
-    vim.keymap.set({ 'n', 'x', 'i' }, '<C-i>', function()
+    vim.keymap.set({ 'x', 'i' }, '<C-i>', function()
       if suggestion.is_visible() then
         suggestion.dismiss()
       else
@@ -26,5 +26,6 @@ return {
     vim.keymap.set({ 'n', 'x', 'i' }, '<C-ø>', suggestion.accept, { desc = 'Accept Copilot suggestion' })
     vim.keymap.set({ 'x', 'i' }, '<C-n>', suggestion.prev, { desc = 'Previous Copilot suggestion' })
     vim.keymap.set({ 'x', 'i' }, '<C-p>', suggestion.next, { desc = 'Next Copilot suggestion' })
+    vim.keymap.set({ 'n' }, '<leader>lk', ':Copilot toggle<CR>', { desc = '[L]lm [K]opilot toggle' })
   end,
 }
