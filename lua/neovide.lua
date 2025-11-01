d5b7e44d5bc445f90e08cb1551be3e3fc7d2cd1d
Simen Holmestad <simen@holmestad.com>
2025-11-01 14:28:28 +0100

    Småting for Neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,24 +1,26 @@
 -- Se https://neovide.dev/configuration.html for mer konfigurasjon
-vim.g.neovide_macos_simple_fullscreen = true
-vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_scroll_animation_length = 0.2
+if vim.g.neovide then
+  vim.g.neovide_macos_simple_fullscreen = true
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0.2
+  vim.g.neovide_hide_mouse_when_typing = true
 
-vim.keymap.set('n', '<D-C-Ø>', function()
-  vim.g.neovide_macos_simple_fullscreen = not vim.g.neovide_macos_simple_fullscreen
-end, { noremap = true, desc = 'Toggle fullscreen' })
+  vim.keymap.set('n', '<D-C-Ø>', function()
+    vim.g.neovide_macos_simple_fullscreen = not vim.g.neovide_macos_simple_fullscreen
+  end, { noremap = true, desc = 'Toggle fullscreen' })
 
-vim.g.neovide_hide_mouse_when_typing = true
-
-vim.g.neovide_scale_factor = 1.0
-local change_scale_factor = function(delta)
-  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-end
-vim.keymap.set({ 'n', 'i' }, '<C-+>', function()
-  change_scale_factor(1.10)
-end, { desc = 'Increase font size' })
-vim.keymap.set({ 'n', 'i' }, '<C-->', function()
-  change_scale_factor(1 / 1.10)
-end, { desc = 'Decrease font size' })
-vim.keymap.set({ 'n', 'i' }, '<C-0>', function()
+  -- Change font size with Ctrl + Plus/Minus/0
   vim.g.neovide_scale_factor = 1.0
-end, { desc = 'Reset scale to default' })
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set({ 'n', 'i' }, '<C-+>', function()
+    change_scale_factor(1.10)
+  end, { desc = 'Increase font size' })
+  vim.keymap.set({ 'n', 'i' }, '<C-->', function()
+    change_scale_factor(1 / 1.10)
+  end, { desc = 'Decrease font size' })
+  vim.keymap.set({ 'n', 'i' }, '<C-0>', function()
+    vim.g.neovide_scale_factor = 1.0
+  end, { desc = 'Reset scale to default' })
+end
