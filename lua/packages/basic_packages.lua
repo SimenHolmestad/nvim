db4c3ff8745b382524d7ea7a66fb9f2aab653f7d
Simen Holmestad <simen@holmestad.com>
2025-12-23 15:29:16 +0100

    Fjerne spider

--- lua/packages/basic_packages.lua
+++ lua/packages/basic_packages.lua
@@ -65,14 +65,6 @@ return {
   },
 
   {
-    'chrisgrieser/nvim-spider',
-    config = function()
-      vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>")
-      vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>")
-      vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>")
-    end,
-  },
-  {
     'chrisgrieser/nvim-various-textobjs',
     opts = {
       keymaps = {
